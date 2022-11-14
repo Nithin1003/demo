378fdf73229b788d994d96856cceecd389f092e5
NithinBabu <nithin@applab.qa>
2022-11-14 16:00:59 +0530

    Adapters created dummy

--- app/src/main/java/uk/ac/tees/W9581934/Admin/Add_ViewDepartments.java
+++ app/src/main/java/uk/ac/tees/W9581934/Admin/Add_ViewDepartments.java
@@ -7,11 +7,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.ac.tees.W9581934.Adapters.DeptAdapter;
+import uk.ac.tees.W9581934.Adapters.DoctorListAdapter;
+import uk.ac.tees.W9581934.Models.DeptModel;
+import uk.ac.tees.W9581934.Models.DoctorListModel;
 import uk.ac.tees.W9581934.R;
 import uk.ac.tees.W9581934.databinding.FragmentAddViewDepartmentsBinding;
 import uk.ac.tees.W9581934.databinding.FragmentDoctorListBinding;
@@ -19,6 +27,8 @@ import uk.ac.tees.W9581934.databinding.FragmentDoctorListBinding;
 
 public class Add_ViewDepartments extends Fragment {
     FragmentAddViewDepartmentsBinding binding;
+    DeptAdapter adapter=new DeptAdapter();
+    List<DeptModel> deptlist = new ArrayList();
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +51,13 @@ public class Add_ViewDepartments extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        for(int i=0;i<10;i++) {
+            deptlist.add(new DeptModel("Cardiology", i));
+        }
+        binding.rvDepartments.setLayoutManager(new LinearLayoutManager(requireContext()));
+        adapter.deptlist=deptlist;
+        binding.rvDepartments.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
         binding.btnFind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
